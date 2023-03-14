58c5734cb1dc11dab6a180c6aec64f69ab7cca01
haripriyanka1 <127424620+haripriyanka1@users.noreply.github.com>
2023-03-14 10:15:49 +0530

    Update getvpcbyid.py

--- getvpcbyid.py
+++ getvpcbyid.py
@@ -1,8 +1,8 @@
 import boto3
 
 # Create a boto3 client for EC2 service
-ec2 = boto3.client('ec2', aws_access_key_id="AKIA42OBUUIF54SCNDGE",
-                   aws_secret_access_key="hzuIUT1XtPYYYYVmL3z8suZQ77bBHDNQi2g0nQl", region_name='us-east-2')
+ec2 = boto3.client('ec2', aws_access_key_id="<<access_key_id>>",
+                   aws_secret_access_key="<<access_key>>", region_name='us-east-2')
 
 # Specify the VPC ID to get details for
 # vpc_id = 'vpc-07ec751cf64a1bd87'
